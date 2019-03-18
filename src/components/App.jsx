b8b9551b8306f494d3414e0ad8f69bf5db2c5127
Leon-Arno <leon.rimrod@gmail.com>
2019-03-18 12:09:57 +0100

    centered element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,19 @@
 import React, { Component } from 'react';
 import { FilePond, registerPlugin } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
-import FilePondPluginPreview from 'filepond-plugin-image-preview';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
 import Header from './Header';
 
-registerPlugin(FilePondPluginPreview);
+registerPlugin(FilePondPluginImagePreview);
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Header />
-        <div className="col-4  m-3">
-          <FilePond
-            allowMultiple={true}
-            style={{ display: 'flex', justifyContent: 'center' }}
-          />
+        <div className="col-4  m-3 mx-auto ">
+          <FilePond allowMultiple={true} />
         </div>
       </div>
     );
