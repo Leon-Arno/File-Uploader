742beae33ed8afb2c9c56c0ac3ac975bfc078423
Leon-Arno <leon.rimrod@gmail.com>
2019-03-18 11:55:54 +0100

    Added dependencies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { FilePond, registerPlugin } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
+import FilePondPluginPreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
 import Header from './Header';
+
+registerPlugin(FilePondPluginPreview);
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Header />
+        <div className="col-4  m-3">
+          <FilePond
+            allowMultiple={true}
+            style={{ display: 'flex', justifyContent: 'center' }}
+          />
+        </div>
       </div>
     );
   }
